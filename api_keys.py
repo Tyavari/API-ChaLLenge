8d36806e63eb610c0eda8bfa75ff570e3f74dd22
Tyavari <83620773+Tyavari@users.noreply.github.com>
2021-09-30 13:08:54 -0400

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+# weather_api_template = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}"
+
+# OpenWeatherMap API Key
+api_key = "Your OpenWeather API Key Here"
+
+# Google Api Key 
+g_key = 'Your Google API Key Here'
